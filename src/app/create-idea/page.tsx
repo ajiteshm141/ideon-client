0b45f1ab3d75597a68f60be451260363df6d9ea4
ajiteshMWFT <ajitesh@mushroomworldbpl.com>
2023-08-08 12:16:41 +0530

    first commit

---
+++ src/app/create-idea/page.tsx
@@ -0,0 +1,204 @@
+"use client";
+
+import React from "react";
+import Select from "react-select";
+const skillOptions: string[] = [];
+const Page = () => {
+  return (
+    <div className="min-h-screen min-w-full flex justify-center items-center">
+      <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+        <div>
+          <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+            Your Background:
+          </h2>
+          <form>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="skills"
+                >
+                  Skills
+                </label>
+                {/* <input /> */}
+                <input
+                  type="text"
+                  id="skills"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="experience"
+                >
+                  Experience
+                </label>
+                <input
+                  id="experience"
+                  type="input"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="Interests"
+                >
+                  Interests
+                </label>
+                <input
+                  id="Interests"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-6">
+              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div>
+          <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+            Market Information:
+          </h2>
+          <form>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="industry"
+                >
+                  Industry
+                </label>
+                {/* <input /> */}
+                <input
+                  type="text"
+                  id="industry"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="trends"
+                >
+                  Trends
+                </label>
+                <input
+                  id="trends"
+                  type="input"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="gaps"
+                >
+                  Industry Gaps
+                </label>
+                <input
+                  id="gaps"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="problems"
+                >
+                  Problems
+                </label>
+                <input
+                  id="problems"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-6">
+              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+        <div>
+          <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
+            Other Details:
+          </h2>
+          <form>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="targetAudience"
+                >
+                  Target Audience
+                </label>
+                {/* <input /> */}
+                <input
+                  type="text"
+                  id="targetAudience"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="trends"
+                >
+                  Trends
+                </label>
+                <input
+                  id="trends"
+                  type="input"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="gaps"
+                >
+                  Industry Gaps
+                </label>
+                <input
+                  id="gaps"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+              <div>
+                <label
+                  className="text-gray-700 dark:text-gray-200"
+                  htmlFor="problems"
+                >
+                  Problems
+                </label>
+                <input
+                  id="problems"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-6">
+              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
+                Save
+              </button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Page;
